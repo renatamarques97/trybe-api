6a9109c62a5359d37e99e37fc5e17006ff5a4e5d
Renata Marques <renata.nrmarques@gmail.com>
2021-02-27 20:37:13 -0300

    add: sessions for users

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class RegistrationsController < Devise::RegistrationsController
   def create
     user = User.new(sign_up_params)
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
       registrations: :registrations,
       sessions: :sessions
     }
+  resources :users
 end
